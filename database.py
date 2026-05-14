3a6f70d0cc1f7f59109a93d22134ef93ae680337
OYanEnrique <enrique4work@gmail.com>
2026-05-13 21:07:13 -0300

    feat: Adiciona mais citações na base de dados

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ topics = [
         "nome_do_artigo": "Evidências, códigos e classificações: o ofício do historiador e o mundo digital",
         "autores": "Fortes, A., & Alvim, L. G. M.",
         "citacao": "A atuação nesse ambiente digital que permeia crescentemente a produção e circulação do conhecimento histórico desafia os pesquisadores a irem além de uma compreensão instrumental e consumista das novas tecnologias.",
-        "pagina": "208",
+        "pagina": "209",
         "ano": 2020,
         "dia_da_leitura": "13/05/2026"
     },
@@ -24,6 +24,60 @@ topics = [
         "citacao": "The aim of this volume is to bring together, in one teaching-focused text, core historical and contemporary reading on the act of defining 'digital humanities' to demonstrate aspects of the history of the field.",
         "pagina": "1",
         "ano": 2013,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_004",
+        "nome_do_artigo": "As Digital Humanities como uma comunidade de práticas: entrevista com o professor Daniel Alves",
+        "autores": "Aquino, I. (Entrevistador), & Alves, D. (Entrevistado)",
+        "citacao": "A adoção destas práticas nas investigações e na interpretação dos dados continuou acontecendo, sem que se utilizasse formalmente esse termo. E é por isso que eu uso o termo comunidade, ou seja, o que nós temos na prática é um conjunto de investigadores, de centros, de estudantes, que incorporaram essas tecnologias no seu método de trabalho...",
+        "pagina": "742",
+        "ano": 2020,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_005",
+        "nome_do_artigo": "Humanidades Digitais e o mundo lusófono",
+        "autores": "Pimenta, R. M., & Alves, D. (Orgs.)",
+        "citacao": "Apresentamos aqui uma produção lusófona do conhecimento nas diversas disciplinas que compõem as Humanidades mediadas pelo uso da computação, dos recursos digitais, seja como ferramenta, como metodologia, seja como efetivo constituinte do objeto de pesquisa.",
+        "pagina": "12",
+        "ano": 2021,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_006",
+        "nome_do_artigo": "Humanities Computing as Digital Humanities",
+        "autores": "Svensson, P.",
+        "citacao": "Together these four articles constitute an attempt to outline and critically discuss how the humanities interrelate with information technology in multiple ways, to understand the historical, conceptual, and disciplinary aspects of this interrelation, and to present an expansive model for the digital humanities.",
+        "pagina": "N/A",
+        "ano": 2009,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_007",
+        "nome_do_artigo": "Selected Definitions from the Day of Digital Humanities: 2009–2012",
+        "autores": "Vários Autores",
+        "citacao": "The wealth of definitions that has been proposed is remarkable both in form and content: they range from the pithy to the bombastic, from the poetical to the enigmatic and from the disruptive to the expositional.",
+        "pagina": "279",
+        "ano": 2012,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_008",
+        "nome_do_artigo": "Something Called Digital Humanities",
+        "autores": "Piez, W.",
+        "citacao": "Digital Humanities is not a mere trend, he argues – it is the Humanities in the digital age.",
+        "pagina": "N/A",
+        "ano": 2008,
+        "dia_da_leitura": "13/05/2026"
+    },
+    {
+        "id": "dh_009",
+        "nome_do_artigo": "Wizards, Bureaucrats, Warriors, and Hackers: Writing the History of the Internet",
+        "autores": "Rosenzweig, R.",
+        "citacao": "The rise of the Net needs to be rooted in the 1960s—in both the 'closed world' of the Cold War and the open and decentralized world of the antiwar movement and the counterculture.",
+        "pagina": "1531",
+        "ano": 1998,
         "dia_da_leitura": "13/05/2026"
     }
     # você pode adicionar outros itens aqui...
